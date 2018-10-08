e8c52c7cb2c710365b7337997c6c6966cbdbf7eb
Jacqueline Nguyen <jnguyen9763@gmail.com>
2018-10-08 02:00:54 -0400

    Changed zoom value

--- mbta/mbta.js
+++ mbta/mbta.js
@@ -2,6 +2,6 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 42.352271, lng: -71.05524200000001},
-        zoom: 8
+        zoom: 10
     });
 }
